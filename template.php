c6bd1cc8497c41e6777ecbe18919be8b43c319e1
Johannes <mail@johannez.com>
2014-08-29 15:24:14 -0700

    Better Wireframing for panels. Added breadcrumb control

--- template.php
+++ template.php
@@ -80,3 +80,53 @@ function mossback_css_alter(&$css) {
     }
 }
 
+/**
+ * Return a themed breadcrumb trail.
+ *
+ * @param $breadcrumb
+ *   An array containing the breadcrumb links.
+ * @return
+ *   A string containing the breadcrumb output.
+ */
+function mossback_breadcrumb($variables) {
+    $breadcrumb = $variables['breadcrumb'];  // Determine if we are to display the breadcrumb.
+    $show_breadcrumb = theme_get_setting('mossback_breadcrumb');
+
+    if ($show_breadcrumb == 'yes' || ($show_breadcrumb == 'admin' && arg(0) == 'admin')) {
+
+        // Optionally get rid of the homepage link.
+        $show_breadcrumb_home = theme_get_setting('mossback_breadcrumb_home');
+        if (!$show_breadcrumb_home) {
+            array_shift($breadcrumb);
+        }
+        // Return the breadcrumb with separators.
+        if (!empty($breadcrumb)) {
+            $breadcrumb_separator = theme_get_setting('mossback_breadcrumb_separator');
+            $trailing_separator = $title = '';
+            if (theme_get_setting('mossback_breadcrumb_title')) {
+                $item = menu_get_item();
+                if (!empty($item['tab_parent'])) {
+                    // If we are on a non-default tab, use the tab's title.
+                    $title = check_plain($item['title']);
+                }
+                else {
+                    $title = drupal_get_title();
+                }
+                if ($title) {
+                    $trailing_separator = $breadcrumb_separator;
+                }
+            }
+            elseif (theme_get_setting('mossback_breadcrumb_trailing')) {
+                $trailing_separator = $breadcrumb_separator;
+            }
+            // Provide a navigational heading to give context for breadcrumb links to
+            // screen-reader users. Make the heading invisible with .element-invisible.
+            $heading = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
+
+            return $heading . '<div class="breadcrumb">' . implode($breadcrumb_separator, $breadcrumb) . $trailing_separator . $title . '</div>';
+        }
+    }
+    // Otherwise, return an empty string.
+    return '';
+}
+
--- theme-settings.php
+++ theme-settings.php
@@ -1,6 +1,50 @@
 <?php
 
 function mossback_form_system_theme_settings_alter(&$form, $form_state) {
+    $form['options_settings']['mossback_breadcrumb'] = array(
+        '#type'          => 'fieldset',
+        '#title'         => t('Breadcrumb settings'),
+        '#attributes'    => array('id' => 'mossback-breadcrumb'),
+    );
+    $form['options_settings']['mossback_breadcrumb']['mossback_breadcrumb'] = array(
+        '#type'          => 'select',
+        '#title'         => t('Display breadcrumb'),
+        '#default_value' => theme_get_setting('mossback_breadcrumb'),
+        '#options'       => array(
+                      'yes'   => t('Yes'),
+                      'admin' => t('Only in admin section'),
+                      'no'    => t('No'),
+                    ),
+    );
+    $form['options_settings']['mossback_breadcrumb']['mossback_breadcrumb_separator'] = array(
+        '#type'          => 'textfield',
+        '#title'         => t('Breadcrumb separator'),
+        '#description'   => t('Text only. Don’t forget to include spaces.'),
+        '#default_value' => theme_get_setting('mossback_breadcrumb_separator'),
+        '#size'          => 5,
+        '#maxlength'     => 10,
+        '#prefix'        => '<div id="div-mossback-breadcrumb-collapse">', // jquery hook to show/hide optional widgets
+    );
+    $form['options_settings']['mossback_breadcrumb']['mossback_breadcrumb_home'] = array(
+        '#type'          => 'checkbox',
+        '#title'         => t('Show home page link in breadcrumb'),
+        '#default_value' => theme_get_setting('mossback_breadcrumb_home'),
+    );
+    $form['options_settings']['mossback_breadcrumb']['mossback_breadcrumb_trailing'] = array(
+        '#type'          => 'checkbox',
+        '#title'         => t('Append a separator to the end of the breadcrumb'),
+        '#default_value' => theme_get_setting('mossback_breadcrumb_trailing'),
+        '#description'   => t('Useful when the breadcrumb is placed just before the title.'),
+    );
+    $form['options_settings']['mossback_breadcrumb']['mossback_breadcrumb_title'] = array(
+        '#type'          => 'checkbox',
+        '#title'         => t('Append the content title to the end of the breadcrumb'),
+        '#default_value' => theme_get_setting('mossback_breadcrumb_title'),
+        '#description'   => t('Useful when the breadcrumb is not placed just before the title.'),
+        '#suffix'        => '</div>', // #div-mossback-breadcrumb
+    );
+
+
     $form['themedev'] = array(
         '#type'          => 'fieldset',
         '#title'         => t('Theme development settings'),
